e1ca0c267436bb012aa8282411f0bc8bd9aa8ab0
Jennifer E <je@jenniferevenson.com>
2018-01-15 03:16:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,4 @@
-// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
-// Key codes for A, B, and C keys.
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
 
 function init() {
   // Write your JavaScript code inside the init() function
