c00db01533eaac3667ead6fbf9dd7a1b602634f9
Jennifer E <je@jenniferevenson.com>
2018-01-15 03:03:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,33 +6,36 @@ function init() {
 const docBody = document.querySelector('body');
  
 docBody.addEventListener('keydown', function(e) {
+    // shows ascii code for the key pressed
     console.log(e.which);
     onKeyDownHandler(e);
-})  // shows ascii code for the key pressed
-
+})  
 } // end function init 
 
 
-const code = [65, 66, 67]; // A B C 
-
+// Key codes for A, B, and C keys.
+const alphabet = [65, 66, 67];
+ 
 // Keep track of index outside of the event handler.
 let index = 0;
-
+ 
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
  
-  if (key === key[index]) {
+  if (key === alphabet[index]) {
     index++;
  
-    if (index === code.length) {
+    if (index === alphabet.length) {
       alert("Hurray!");
+ 
       index = 0;
     }
   } else {
     index = 0;
   }
 }
+
 
 /* 
 konami = 0;	
