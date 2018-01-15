f30f2c83cbd85a54a35c5732c59dc0befa09bad8
Jennifer E <je@jenniferevenson.com>
2018-01-15 03:13:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,8 @@
 // const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
+// Key codes for A, B, and C keys.
+const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 
 function init() {
   // Write your JavaScript code inside the init() function
@@ -14,9 +17,6 @@ docBody.addEventListener('keydown', function(e) {
 } // end function init 
 
 
-// Key codes for A, B, and C keys.
-const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
- 
 // Keep track of index outside of the event handler.
 let index = 0;
  
